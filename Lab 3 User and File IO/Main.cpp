106290ff25e15ed13e36a6ab26b3d24895d05c10
salmanmani092 <70169067+salmanmani092@users.noreply.github.com>
2021-09-29 21:39:32 -0500

    Final but not final

--- Lab 3 User and File IO/Main.cpp
+++ Lab 3 User and File IO/Main.cpp
@@ -3,7 +3,8 @@ Muhammad Salman
 Fall 2021
 Due: September 15
 Lab 3: User and File I/O
-Description: To learn how to creat and input values in a file and retrieve that info and calculate it and save it into another file.
+Description: To learn how to creat a .dat file and input values in it. And then retrieve that info and calculate it and save it into another file in
+the same folder.
 */
 
 
@@ -14,38 +15,52 @@ Description: To learn how to creat and input values in a file and retrieve that 
 
 using namespace std;
 
+float standardDev(float varstr[])    // using code for calculating standard deviation from https://www.youtube.com/watch?v=TeM7-35RVbg
+{
+	float mean, sum = 0, SD = 0;
+	int m;
+	for (m = 0; m < 4; ++m)
+	{
+		sum += varstr[m];
+	}
+	mean = sum / 4;
+	for (m = 0; m < 4; ++m)
+		SD += pow(varstr[m] - mean, 2);
+	return sqrt(SD / 4);                       // completing standard deviation calculations
+}
 int main()
 {
-	int data1;
-	int data2;
-	int data3;
-	int data4;
-	int i;
-	float data[4];
-
+	float data1;   // declaring values from inMeanStd.dat
+	float data2;
+	float data3;
+	float data4;
 
 	string inputfilename = "inMeanStd.dat";   // Assuming inMeanStd.data is in the same folder
-	string outputfilename = "outMeanStd.dat";  // 
+	string outputfilename = "outMeanStd.dat";  //  output file created in the same folder
 
-	ifstream infile;
+	ifstream infile;          // declaring in and out files
 	ofstream outfile;
-	infile.open(inputfilename);
+	infile.open(inputfilename);                // opening both input and output files
 	outfile.open(outputfilename);
 
 	infile >> data1 >> data2 >> data3 >> data4;
 	outfile << data1 << data2 << data3 << data4;
 
-	float mean = (data1 + data2 + data3 + data4) / 4;
+	float mean = data1 + data2 + data3 + data4 / 4;   // calculating mean with values in the file.
 
+	int k;
+	float varstr[40];
+	cout << "Input your four values here: ";     // asking user to input values for standard deviation calculations
+	for (k = 0; k < 4; ++k)
+		cin >> varstr[k];
+
+	cout << endl << "Standard Deviation for your values is: " << standardDev(varstr) << endl;  // Displaying Standard Deviation from values input by user	
+	cout << "Your data values for Mean are:" << data1 << ","  << data2<< "," << data3 << ", and "  << data4 << endl;  // data values inputed in the file.
+	cout << "Your Mean is: " << mean << endl;      // displaying result of mean calculation on screen.
 	
-	
-
-	cout << "The data values are:" << data1 << ","  << data2<< "," << data3 << ", and "  << data4 << endl;
-	cout << "Your Mean is: " << mean << endl;
-	
 
 
-	infile.close();
+	infile.close();       // closing files.
 	outfile.close();
 
 	
